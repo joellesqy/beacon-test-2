dd7f02996b02c04cb1260b0b3b29a2a930115537
Joelle Sim <1306118i@student.tp.edu.sg>
2015-12-02 11:32:25 +0800

    trying codes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,41 @@
-// The app object is defined in a closure to make it easier to
-// change its visible name, if that would be needed.
+// Define application object and common functions.
 var app = (function()
 {
 	// Application object.
 	var app = {};
 
-	// Touch event state.
-	var touchIsActive = false
-	var touchStartX = 0
-	var touchStartY = 0
-
 	// Application data.
 	app.currentScreenId = null;
 	app.beaconColorStyles = [
-		'style-color-unknown style-color-unknown-text',
-		'style-color-mint style-color-mint-text',
-		'style-color-ice style-color-ice-text',
-		'style-color-blueberry-dark style-color-blueberry-dark-text',
-		'style-color-white style-color-white-text',
-		'style-color-transparent style-color-transparent-text'];
+		'style-color-unknown style-color-unknown-text',        // BeaconColorUnknown
+		'style-color-mint style-color-mint-text',              // BeaconColorMintCocktail
+		'style-color-ice style-color-ice-text',                // BeaconColorIcyMarshmallow
+		'style-color-blueberry-dark style-color-blueberry-dark-text', // BeaconColorBlueberryPie
+		'style-color-unknown style-color-unknown-text',        // TODO: BeaconColorSweetBeetroot
+		'style-color-unknown style-color-unknown-text',        // TODO: BeaconColorCandyFloss
+		'style-color-unknown style-color-unknown-text',        // TODO: BeaconColorLemonTart
+		'style-color-unknown style-color-unknown-text',        // TODO: BeaconColorVanillaJello
+		'style-color-unknown style-color-unknown-text',        // TODO: BeaconColorLiquoriceSwirl
+		'style-color-white style-color-white-text',            // BeaconColorWhite
+		'style-color-transparent style-color-transparent-text' // BeaconColorTransparent
+		];
 	app.proximityNames = [
 		'unknown',
 		'immediate',
 		'near',
 		'far'];
 
-	// ------------- Private helper functions ------------- //
+	// ------------- Private helper function ------------- //
 
 	function onDeviceReady()
 	{
 		// TODO: Add functionality if needed.
+		hyper.log('onDeviceReady')
 	}
 
-	function checkTouchActive()
-	{
-		var active = touchIsActive;
-		touchIsActive = false;
-		return active;
-	}
+	// ------------- Application functions ------------- //
 
-	function formatDistance(meters)
+	app.formatDistance = function(meters)
 	{
 		if (!meters) { return 'Unknown'; }
 
@@ -51,9 +47,9 @@ var app = (function()
 		{
 			return (meters * 100).toFixed(3) + ' cm';
 		}
-	}
+	};
 
-	function formatProximity(proximity)
+	app.formatProximity = function(proximity)
 	{
 		if (!proximity) { return 'Unknown'; }
 
@@ -63,9 +59,9 @@ var app = (function()
 
 		// Return name for proximity.
 		return app.proximityNames[proximity];
-	}
+	};
 
-	function beaconColorStyle(color)
+	app.beaconColorStyle = function(color)
 	{
 		if (!color)
 		{
@@ -78,9 +74,7 @@ var app = (function()
 
 		// Return style class for color.
 		return app.beaconColorStyles[color];
-	}
-
-	// ------------- Public application functions ------------- //
+	};
 
 	app.showScreen = function(screenId)
 	{
@@ -101,274 +95,9 @@ var app = (function()
 		app.showScreen('id-screen-home');
 	};
 
-	app.showScanScreen = function()
-	{
-		app.showScreen('id-screen-scan');
-	};
-
-	app.showRangeScreen = function()
-	{
-		app.showScreen('id-screen-range');
-	};
-
-	app.showMonitorScreen = function()
-	{
-		app.showScreen('id-screen-monitor');
-	};
-
-	app.startScanning = function()
-	{
-		function onScan(beaconInfo)
-		{
-			//console.log('onScan');
-			displayBeconInfo(beaconInfo);
-		}
-
-		function onError(errorMessage)
-		{
-			console.log('Scan error: ' + errorMessage);
-		}
-
-		function displayBeconInfo(beaconInfo)
-		{
-			//console.log('displayBeconInfo');
-			// Clear beacon HTML items.
-			$('#id-screen-scan .style-beacon-list').empty();
-
-			// Sort beacons by signal strength.
-			beaconInfo.beacons.sort(function(beacon1, beacon2) {
-				return beacon1.rssi > beacon2.rssi; });
-
-			// Generate HTML for beacons.
-			var html = '';
-			$.each(beaconInfo.beacons, function(key, beacon)
-			{
-				// jQuery doesn't work.
-				var element = $(createBeaconHTML(beacon));
-				$('#id-screen-scan .style-beacon-list').append(element);
-			});
-		};
-
-		function createBeaconHTML(beacon)
-		{
-			console.log('beacon: '+beacon.major+' '+beacon.minor+', '+beacon.rssi+' ('+beacon.measuredPower+')');
-			var colorClasses = beaconColorStyle(beacon.color);
-			htm = '<div class="' + colorClasses + '">'
-				+ '<table><tr><td>Major</td><td>' + beacon.major
-				+ '</td></tr><tr><td>Minor</td><td>' + beacon.minor
-				+ '</td></tr><tr><td>RSSI</td><td>' + beacon.rssi
-				+ '</td></tr><tr><td>Measured power</td><td>' + beacon.measuredPower
-				+ '</td></tr><tr><td>MAC address</td><td>' + beacon.macAddress
-				+ '</td></tr></table></div>';
-			return htm;
-		};
-
-		app.showScanScreen();
-
-		console.log("startEstimoteBeaconsDiscoveryForRegion")
-
-		EstimoteBeacons.startEstimoteBeaconsDiscoveryForRegion(
-			{}, // Empty region matches all beacons.
-			onScan,
-			onError);
-	};
-
-	app.stopScanning = function()
-	{
-		console.log("stopEstimoteBeaconDiscovery")
-		EstimoteBeacons.stopEstimoteBeaconDiscovery();
-		app.showHomeScreen();
-	};
-
-	app.startRanging = function()
-	{
-		function onRange(beaconInfo)
-		{
-			console.log('onRange');
-			displayBeconInfo(beaconInfo);
-		}
-
-		function onError(errorMessage)
-		{
-			console.log('Range error: ' + errorMessage);
-		}
-
-		function displayBeconInfo(beaconInfo)
-		{
-			// Clear beacon HTML items.
-			$('#id-screen-range .style-beacon-list').empty();
-
-			// Sort beacons by distance.
-			beaconInfo.beacons.sort(function(beacon1, beacon2) {
-				return beacon1.distance > beacon2.distance; });
-
-			// Generate HTML for beacons.
-			$.each(beaconInfo.beacons, function(key, beacon)
-			{
-				var element = $(createBeaconHTML(beacon));
-				$('#id-screen-range .style-beacon-list').append(element);
-			});
-		};
-
-		function createBeaconHTML(beacon)
-		{
-			var colorClasses = beaconColorStyle(beacon.color);
-			htm = '<div class="' + colorClasses + '">'
-				+ '<table><tr><td>Major</td><td>' + beacon.major
-				+ '</td></tr><tr><td>Minor</td><td>' + beacon.minor
-				+ '</td></tr><tr><td>RSSI</td><td>' + beacon.rssi
-			if (beacon.proximity)
-			{
-				htm += '</td></tr><tr><td>Proximity</td><td>'
-					+ formatProximity(beacon.proximity)
-			}
-			if (beacon.distance)
-			{
-				htm += '</td></tr><tr><td>Distance</td><td>'
-					+ formatDistance(beacon.distance)
-			}
-			htm += '</td></tr></table></div>';
-			return htm;
-		};
-
-		app.showRangeScreen();
-
-		EstimoteBeacons.requestAlwaysAuthorization();
-
-		EstimoteBeacons.startRangingBeaconsInRegion(
-			{}, // Empty region matches all beacons.
-			onRange,
-			onError);
-	};
-
-	app.stopRanging = function()
-	{
-		EstimoteBeacons.stopRangingBeaconsInRegion({});
-		app.showHomeScreen();
-	};
-
-	app.startMonitoring = function()
-	{
-		function onMonitor(regionState)
-		{
-			displayRegionInfo(regionState);
-		}
-
-		function onError(errorMessage)
-		{
-			console.log('Monitor error: ' + errorMessage);
-		}
-
-		// TODO: Handle multiple regions. Currently only
-		// one region will be displayed.
-		function displayRegionInfo(regionState)
-		{
-			$('#id-screen-monitor .style-beacon-list').empty();
-
-			var element = $(createRegionStateHTML(regionState));
-			$('#id-screen-monitor .style-beacon-list').append(element);
-		};
-
-		function createRegionStateHTML(regionState)
-		{
-			var color = 'style-color-pink';
-			htm = '<div class="' + color + '">'
-				+ '<table><tr><td>State</td><td>' + regionState.state
-				+ '</td></tr><tr><td>Major</td><td>' + regionState.major
-				+ '</td></tr><tr><td>Minor</td><td>' + regionState.minor
-				+ '</td></tr><tr><td>Identifier</td><td>' + regionState.identifier
-				+ '</td></tr><tr><td>UUID</td><td>' + regionState.uuid
-				+ '</td></tr></table></div>';
-			return htm;
-		};
-
-		app.showMonitorScreen();
-
-		EstimoteBeacons.requestAlwaysAuthorization();
-
-		EstimoteBeacons.startMonitoringForRegion(
-			{}, // Empty region matches all beacons.
-			onMonitor,
-			onError);
-	};
-
-	app.stopMonitoring = function()
-	{
-		EstimoteBeacons.stopMonitoringForRegion({});
-		app.showHomeScreen();
-	};
-
-	// ------------- Public touch event functions ------------- //
-
-	app.onStartScanning = function()
-	{
-		console.log('app.onStartScanning1');
-		if (!checkTouchActive()) { return }
-		console.log('app.onStartScanning2');
-		app.startScanning();
-	};
-
-	app.onStopScanning = function()
-	{
-		if (!checkTouchActive()) { return }
-		app.stopScanning();
-	};
-
-	app.onStartRanging = function()
-	{
-		if (!checkTouchActive()) { return }
-		app.startRanging();
-	};
-
-	app.onStopRanging = function()
-	{
-		if (!checkTouchActive()) { return }
-		app.stopRanging();
-	};
-
-	app.onStartMonitoring = function()
-	{
-		if (!checkTouchActive()) { return }
-		app.startMonitoring();
-	};
-
-	app.onStopMonitoring = function()
-	{
-		if (!checkTouchActive()) { return }
-		app.stopMonitoring();
-	};
-
 	app.onNavigateBack = function()
 	{
-		if (!checkTouchActive()) { return }
 		history.back();
-	};
-
-	app.onTouchStart = function(event)
-	{
-		var changedTouch = event.changedTouches[0];
-		touchStartX = changedTouch.screenX
-		touchStartY = changedTouch.screenY;
-		touchIsActive = true;
-	};
-
-	app.onTouchMove = function(event)
-	{
-		if (touchIsActive)
-		{
-			var changedTouch = event.changedTouches[0];
-			var touchDeltaX = Math.abs(changedTouch.screenX - touchStartX);
-			var touchDeltaY = Math.abs(changedTouch.screenY - touchStartY);
-			if (touchDeltaX > 5 || touchDeltaY > 5)
-			{
-				touchIsActive = false;
-			}
-		}
-	};
-
-	app.onTouchCancel = function(event)
-	{
-		touchIsActive = false;
 	};
 
 	// ------------- Initialisation ------------- //
